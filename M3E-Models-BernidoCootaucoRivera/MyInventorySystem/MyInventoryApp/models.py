e9c9d0b1ed69ed2e7d45380cfa919a52564df5a3
miguelgeoffreycootauco-oss <miguel.geoffrey.cootauco@student.ateneo.edu>
2026-02-13 17:26:14 +0800

    Supplier model done

--- M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
-### Test
-#helo
+class Supplier(models.Model):
+
+    name = models.CharField(max_length=300)
+    city = models.CharField(max_length=300)
+    country = models.CharField(max_length=300)
+    created_at = models.DateTimeField(blank=True, null = True)
+    objects = models.Manager()
+    
+    def getName(self):
+        return self.name
+
+    def __str__(self):
+        return f"{self.name}, {self.city}, {self.country} (created at: {self.created_at})"
