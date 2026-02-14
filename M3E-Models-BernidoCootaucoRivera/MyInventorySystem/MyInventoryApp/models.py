1e7ea2720a045e2b3c8b8905bafb4f32e433d6da
bonbernido <menediobernido@gmail.com>
2026-02-14 18:20:20 +0800

    waterbottle2

--- M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/models.py
+++ M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/models.py
@@ -16,3 +16,16 @@ class Supplier(models.Model):
 
     def __str__(self):
         return f"{self.name}, {self.city}, {self.country} (created at: {self.created_at})"
+    
+class WaterBottle(models.Model):
+    sku = models.CharField(max_length=3, unique=True)
+    brand = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=100)
+    mouth_size = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    supplied_by = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    current_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.sku}: {self.brand}, {self.mouth_size}, {self.size}, {self.color}, supplied by {self.supplied_by.name}, {self.cost} : {self.current_quantity}"
