6879d57e28a781b372bcbac60a345ccf2dee57f2
bonbernido <menediobernido@gmail.com>
2026-02-14 18:21:20 +0800

    waterbottle admin.py

--- M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/admin.py
+++ M3E-Models-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Supplier
+from .models import Supplier, WaterBottle
 admin.site.register(Supplier)
+admin.site.register(WaterBottle)
