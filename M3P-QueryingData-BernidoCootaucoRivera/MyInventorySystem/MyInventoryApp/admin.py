ede2d63c58017e9491e997a32c976ddda29b34ae
miguelgeoffreycootauco-oss <miguel.geoffrey.cootauco@student.ateneo.edu>
2026-02-20 13:08:35 +0800

    done

--- M3P-QueryingData-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/admin.py
+++ M3P-QueryingData-BernidoCootaucoRivera/MyInventorySystem/MyInventoryApp/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 from .models import Supplier, WaterBottle
 admin.site.register(Supplier)
 admin.site.register(WaterBottle)
+
+### All group created supplier, quantity of 5 or more, price is 1000-3000
+### WaterBottle.objects.filter(supplied_by__name__in=["BF Inc.", "Abbott, Inc", "MGC Inc"], current_quantity__gte=5, cost__range=(1000, 3000))
